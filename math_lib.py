a377b7b78e667cce12c8f2ed5760bf8bd08478cd
alisoncleonard <alisoncleonard@gmail.com>
2019-09-02 21:43:36 -0600

    added multiply, add, and subtract functions

--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,15 @@
+# library of basic math functions
 def div(a, b):
     return a/b
+
+def multiply(a, b):
+    return a*b
+
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+
+    
