4c96ffcb883126df2747900c43fff33335f7af5c
alisoncleonard <alisoncleonard@gmail.com>
2019-09-03 20:14:04 -0600

    fixed div exception

--- math_lib.py
+++ math_lib.py
@@ -1,15 +1,16 @@
 # library of basic math functions
+
+
 def div(a, b):
-    return a/b
+    if b == 0:
+        return None
+    else:
+        return a/b
+
 
 def multiply(a, b):
     return a*b
 
+
 def add(a, b):
     return a+b
-
-def subtract(a, b):
-    return a-b
-
-
-    
